02c2c875e89b609e65c930a792110a3fb047e9c9
Nate Annau <nateannau@gmail.com>
2026-01-07 22:05:27 -0800

    Fallthrough on symbols layer

--- keyboards/lily58/keymap.c
+++ keyboards/lily58/keymap.c
@@ -70,9 +70,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                   `----------------------------'           '------''--------------------'
  */
 [_SYM] = LAYOUT(
-  KC_F1,       KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                        KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
-  KC_TILD,     KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                      KC_CIRC, KC_AMPR, KC_ASTR, KC_P7,   KC_P8,   KC_P9,
-  KC_QUES,     KC_LPRN, KC_RPRN, KC_LCBR, KC_RCBR, KC_PLUS,                      KC_EQL,  KC_COLN, KC_DQUO, KC_P4,   KC_P5,   KC_P6,
-  XXXXXXX,     XXXXXXX, KC_PIPE, KC_LBRC, KC_RBRC, KC_UNDS, LCTL(KC_A), XXXXXXX, KC_LT,   KC_GT,   KC_P0,   KC_P1,   KC_P2,   KC_P3,
-                                 _______, _______, _______, _______,    _______, _______, _______, _______
+  KC_F1,       KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
+  KC_TILD,     KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                   KC_CIRC, KC_AMPR, KC_ASTR, KC_P7,   KC_P8,   KC_P9,
+  KC_QUES,     KC_LPRN, KC_RPRN, KC_LCBR, KC_RCBR, KC_PLUS,                   KC_EQL,  KC_COLN, KC_DQUO, KC_P4,   KC_P5,   KC_P6,
+  XXXXXXX,     XXXXXXX, KC_PIPE, KC_LBRC, KC_RBRC, KC_UNDS, _______, _______, KC_LT,   KC_GT,   KC_P0,   KC_P1,   KC_P2,   KC_P3,
+                                 _______, _______, _______, _______, _______, _______, _______, _______
 )};
